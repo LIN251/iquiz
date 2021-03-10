3fcd527125c61d7c67429bf7fbdc8a2e15c2269f
LIN251 <lz2304@vt.edu>
2021-03-09 21:25:47 -0500

    remove

--- src/main/java/edu/vt/iQuiz_Group3/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package edu.vt.iQuiz_Group3;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
